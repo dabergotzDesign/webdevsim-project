6e7642cec98d85970fe7793db66f92c50092535f
DCI-REC102 <otakulp@gmx.de>
2020-01-15 14:50:13 +0100

    fixed javascript

--- game/WDSprotosite/main.js
+++ game/WDSprotosite/main.js
@@ -1,6 +1,9 @@
 
 // Change image when buttons is clicked
 let image = document.querySelector(".galleryImage");
+const imageTitle = document.querySelector(".imageTitle");
+const imageDescription = document.querySelector(".imageDescription");
+
 const galleryButtons = document.querySelectorAll(".galleryButton");
 
 const gBtn1 = document.querySelector(".galleryButton1");
@@ -11,14 +14,23 @@ image.src="images/image1.png";
 
 gBtn1.addEventListener("click",()=>{
     image.src="images/image1.png";
+    imageTitle.innerHTML="Title 01";
+    
+    imageDescription.innerHTML="Cool Image isn't it?";
 });
 
 gBtn2.addEventListener("click",()=>{
     image.src="images/image2.png";
+    imageTitle.innerHTML="Title 02";
+
+    imageDescription.innerHTML="Another one";
 });
 
 gBtn3.addEventListener("click",()=>{
     image.src="images/image3.png";
+    imageTitle.innerHTML="Title 03";
+
+    imageDescription.innerHTML="Feels like the empire joined.";
 });
 
 
