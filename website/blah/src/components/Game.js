ef0eaf49531c41338fcab4c65d048804205bb375
KasaBerlin <katrinsalac@gmail.com>
2020-01-27 09:27:02 +0100

    add

--- website/blah/src/components/Game.js
+++ website/blah/src/components/Game.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import GameWindow from "../components/MyGame/Unity";
 
 const Game =()=>{
 return(
     <section className="game">
     <div className="game-frame">
+        <GameWindow/>
     </div>
     <a id="download" href="empty">download here...</a>
     </section>
