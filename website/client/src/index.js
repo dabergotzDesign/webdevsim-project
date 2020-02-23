af48a20edf1dc7cc32f9f777ab0797ab4cd33021
pudelgulasch <katrinsalac@gmail.com>
2020-02-24 00:18:00 +0100

    rm node_modules

--- website/client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from "./components/Router";
-import './index.css';
-
-import "@fortawesome/fontawesome-free/css/all.css";
-
-ReactDOM.render(<Router/>, document.getElementById('root'));
